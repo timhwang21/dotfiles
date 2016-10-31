53305bf8f825caf3ad4ac303cdf2806c1c15afa2
Tim Hwang <thwang@rocketfuel.com>
2016-10-31 13:03:01 -0400

    Fix cp command dealing with spaces in filenames

--- setup.sh
+++ setup.sh
@@ -4,13 +4,13 @@
 shopt -s dotglob
 
 # symlink dotfiles
-for filename in $HOME/dotfiles/settings/*; do
-  ln -fs $filename $HOME/$(basename $filename)
+for filename in settings/*; do
+  ln -fs "$filename" "$HOME/$(basename "$filename")"
 done
 
 # copy sublime settings
-for filename in $HOME/dotfiles/sublime/*; do
-  cp -i $filename $HOME/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/$(basename $filename)
+for filename in sublime/*; do
+  cp -i "$filename" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/$(basename "$filename")"
 done
 
 # turn off detect hidden files
