bb2acc7566ec88dca891d2d1a71031ca0c491086
timhwang21 <timhwang21@gmail.com>
2018-10-14 22:13:41 -0400

    Automate sublime package installation

--- README.md
+++ README.md
@@ -16,32 +16,6 @@ chmod +x bootstrap.sh
 sh bootstrap.sh
 ```
 
-## Sublime Packages
-
-As far as I know there is no good way to programmatically install Sublime packages, so a list will be maintained here:
-
-* AdvancedNewFile
-* Babel
-* BracketHighlighter
-* ColorHelper
-* Focus File on Sidebar
-* Git
-* GitGutter
-* Markdown Extended
-* Markdown HTML Preview
-* Monokai Extended
-* nginx
-* Package Control
-* Sass
-* SideBarEnhancements
-* SublimeLinter
-* SublimeLinter-annotations
-* SublimeLinter-eslint
-* SublimeLinter-ghc
-* SublimeLinter-ruby
-* SublimeLinter-tsling
-* TypeScript
-
 ## MacOS Applications
 
 * 2do
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-SUBL_PACKAGE_PATH="Library/Application Support/Sublime Text 3/Packages/User"
+SUBL_PATH="Library/Application Support/Sublime Text 3"
 
 # detect hidden files
 shopt -s dotglob
@@ -16,9 +16,12 @@ for filename in sources/*; do
   ln -fs "$DIR/$filename" "$HOME/$(basename "$filename")"
 done
 
+# copy sublime package control; will install packages on initial run
+cp -i "Package Control.sublime-package" "$HOME/$SUBL_PATH/Installed Packages/Package Control.sublime-package"
+
 # copy sublime settings
 for filename in sublime/*; do
-  cp -i "$filename" "$HOME/$SUBL_PACKAGE_PATH/$(basename "$filename")"
+  cp -i "$filename" "$HOME/$SUBL_PATH/Packages/User/$(basename "$filename")"
 done
 
 # turn off detect hidden files
