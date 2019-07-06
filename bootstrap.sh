70094a84c5f8fd2bdb80cdfc1dd9eef39f6939bb
timhwang21 <timhwang21@gmail.com>
2019-07-06 12:09:08 +0800

    Remove sublime stuff from bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-SUBL_PATH="Library/Application Support/Sublime Text 3"
 
 # detect hidden files
 shopt -s dotglob
@@ -14,14 +13,6 @@ done
 # symlink sources
 for filename in sources/*; do
   ln -fs "$DIR/$filename" "$HOME/$(basename "$filename")"
-done
-
-# copy sublime package control; will install packages on initial run
-cp -i "Package Control.sublime-package" "$HOME/$SUBL_PATH/Installed Packages/Package Control.sublime-package"
-
-# copy sublime settings
-for filename in sublime/*; do
-  cp -i "$filename" "$HOME/$SUBL_PATH/Packages/User/$(basename "$filename")"
 done
 
 # turn off detect hidden files
